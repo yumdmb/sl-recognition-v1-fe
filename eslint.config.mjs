86e5b50885a6986dc6844bc23469f2ae2d6f38b2
yumdmb <elvissawing.muran@gmail.com>
2025-12-17 01:18:00 +0800

    use eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      '.next/**',
+      'node_modules/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+    ],
+  },
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+  }),
 ];
 
 export default eslintConfig;
